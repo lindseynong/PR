47dd951da39c963cec4ffc7f15b4c9de85a4c947
lindseynong <afqsgasd12@mail.com>
2024-09-03 12:00:00 +0800

    test(junit): add setup/teardown and assertion to Facebook login test

--- src/TestFacebookWithJUnit.java
+++ src/TestFacebookWithJUnit.java
@@ -1,4 +1,3 @@
-
 import org.junit.Test;
 import org.junit.After;
 import org.junit.Before;
@@ -8,33 +7,38 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 import static org.junit.Assert.*;
+
 public class TestFacebookWithJUnit {
-static WebDriver driver;
-FirefoxOptions a = new FirefoxOptions();
-public abstract class MyTestBaseClass {}
-@Before
-public void setUp() throws Exception{
-	
+    static WebDriver driver;
+    FirefoxOptions options = new FirefoxOptions();
 
-}
-@After
-public void tearDown() throws Exception{
-	
-}
-	public
-	@Test
-	void test() {
-		System.setProperty("webdriver.gecko.driver", "C:\\Users\\Computer\\Desktop\\geckodriver.exe");
-		a.addPreference("browser.tabs.remote.autostart", false);
-		driver=new FirefoxDriver(a);
-		driver.get("http://facebook.com");
-		driver.manage().window().maximize();
-		driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("iasjdiaosjd@gmail.com");
-		driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("Asdasd123!");
-		driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
-		
-		
-		
-	}
+    @Before
+    public void setUp() throws Exception {
+        System.setProperty("webdriver.gecko.driver", "C:\\Users\\Computer\\Desktop\\geckodriver.exe");
+        options.addPreference("browser.tabs.remote.autostart", false);
+        driver = new FirefoxDriver(options);
+        driver.manage().window().maximize();
+    }
 
+    @After
+    public void tearDown() throws Exception {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void testFacebookLoginForm() {
+        driver.get("http://facebook.com");
+
+        driver.findElement(By.xpath(".//*[@id='email']")).sendKeys("iasjdiaosjd@gmail.com");
+        driver.findElement(By.xpath(".//*[@id='pass']")).sendKeys("Asdasd123!");
+        driver.findElement(By.xpath(".//*[@id='u_0_2']")).click();
+
+        // 🆕 Assertion: Check if an expected element exists (example: an error message or stay on login page)
+        boolean isLoginErrorDisplayed = driver.findElements(By.xpath("//*[contains(text(), 'incorrect')]")).size() > 0;
+
+        // 🧪 Assertion placeholder
+        assertTrue("Expected login error message is not displayed.", isLoginErrorDisplayed);
+    }
 }
